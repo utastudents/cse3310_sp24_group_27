01f7142d2c45197cbe43cf8122e6e5d99a715ae7
adrianm104 <122935270+adrianm104@users.noreply.github.com>
2024-03-21 22:32:01 +0000

    added basic functionality layout

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -6,13 +6,7 @@ public class UserEvent {
     PlayerType PlayerIdx; // either an XPLAYER or an OPLAYER
     int Button; // button number from 0 to 8
 
-    UserEvent() {
-
-    }
-
     UserEvent(int _GameId, PlayerType _PlayerIdx, int _Button) {
-        GameId = _GameId;
-        PlayerIdx = _PlayerIdx;
-        Button = _Button;
+
     }
 }
