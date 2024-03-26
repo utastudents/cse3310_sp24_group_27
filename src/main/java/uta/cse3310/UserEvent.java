9e10f2d6437af790c741fdcea4248b068b67e4f5
adrianm104 <122935270+adrianm104@users.noreply.github.com>
2024-03-26 22:21:53 +0000

    updated methods

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -6,7 +6,14 @@ public class UserEvent {
     PlayerType PlayerIdx; // either an XPLAYER or an OPLAYER
     int Button; // button number from 0 to 8
 
-    UserEvent(int _GameId, PlayerType _PlayerIdx, int _Button) {
 
+    UserEvent() {
+
+    }
+
+    UserEvent(int _GameId, PlayerType _PlayerIdx, int _Button) {
+        GameId = _GameId;
+        PlayerIdx = _PlayerIdx;
+        Button = _Button;
     }
 }
