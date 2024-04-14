db47e1f81cb48275cd46a7977cb2fa800c62817e
MED3500 <143138523+MED3500@users.noreply.github.com>
2024-04-13 21:48:53 -0500

    starting to fill in generateGrid, created getWord

--- src/main/java/uta/cse3310/WordGrid.java
+++ src/main/java/uta/cse3310/WordGrid.java
@@ -1,15 +1,70 @@
 package uta.cse3310;
 import java.util.List;
+import java.util.Random;
+
 
 public class WordGrid {
-   private Integer size;
-   private Double density; 
-   private List<String> words;
-   private char[][] grid;
 
-   
-   public void generateGrid(Integer size, Double density, List<String> words, char[][] grid){
+   Random rand = new Random();
+
+   // generate both grids (asteriskGrid and shownGrid)
+   public void generateGrid(int gridSize, double numWords, List<String> words, String[][] asteriskGrid, String[][] shownGrid){
+      
+      /* 
+         key to the shown grid, ex: * * * * * w o r d * *
+            char asteriskGrid[gridSize][gridSize]
+
+         the word grid actually shown to the players ex: h j l o y w o r d q r
+            char shownGrid[gridSize][gridSize]
+      */
+
+      // initial fill of the asteriskGrid with * in every spot
+      for(int i = 0; i < gridSize; i++){
+         for(int k = 0; k < gridSize; k++){
+            asteriskGrid[i][k] = "*";
+         }
+      }
+
+      for(int i = 0; i < gridSize; i++) {
+         for(int k = 0; k < gridSize; k++){
+            for(int p = 1; p <= numWords; p++){
+               // String word = getWord();
+
+               // up, down, right, upright, downright
+               int direction = rand.nextInt(4);
+
+               // switch(direction) {
+               //    case 0: 
+               // }
+               
+            }
+         }
+      }
 
    }
 
+   public String getWord() {
+      // int randNum = rand.nextInt(25);
+      // String word;
+
+
+      // while(!EOF) {
+      //    if(getchar(line) == startLetter) {
+      //       if(i == index)
+      //          return line;
+      //       else {
+      //          index++;
+      //       }
+
+      //    }
+      // }
+      return "a";
+   }
+
+   public static void main(String[] args){
+      // generateGrid(50, 10, )
+      // System.out.print("k");
+   }
+
 }
+
