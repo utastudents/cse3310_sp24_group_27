0de4f4a405d55bc666198535506ce50f61c27380
adrianm104 <122935270+adrianm104@users.noreply.github.com>
2024-04-14 22:53:29 +0000

    added statistics functionality with some adjustments

--- src/main/java/uta/cse3310/Statistics.java
+++ src/main/java/uta/cse3310/Statistics.java
@@ -11,20 +11,31 @@ public class Statistics {
     //Keeping these fields
     private Integer player1Wins;
     private Integer player2Wins;
+    private Integer player3Wins;
+    private Integer player4Wins;
     private Integer Draws;
     private Integer GamesInProgress;
     //The new fields are below this 
     private Integer totalScore;
-    private Integer playerRank;
-
+    private Integer player1Rank;
+    private Integer player2Rank;
+    private Integer player3Rank;
+    private Integer player4Rank;
 
 
 
     public Statistics() {
         player1Wins = 0;
         player2Wins = 0;
+        player3Wins = 0;
+        player4wins = 0;
         Draws = 0;
         GamesInProgress = 0;
+        player1Rank = 0;
+        player2Rank = 0;
+        player3Rank = 0;
+        player4Rank = 0;
+        totalScore = 0;
     }
     public Integer Leader_board(){
         return 0;
@@ -34,7 +45,7 @@ public class Statistics {
     }
 
     public void setPlayer1Wins(Integer player1Wins) {
-        player1Wins = player1Wins;
+        this.player1Wins = player1Wins;
     }
 
     public Integer getPlayer2Wins() {
@@ -42,7 +53,23 @@ public class Statistics {
     }
 
     public void setPlayer2Wins(Integer player2Wins) {
-        player2Wins = player2Wins;
+        this.player2Wins = player2Wins;
+    }
+
+    public Integer getPlayer3Wins() {
+        return player3Wins;
+    }
+
+    public void setPlayer3Wins(Integer player3Wins) {
+        this.player3Wins = player3Wins;
+    }
+
+    public Integer getPlayer4Wins() {
+        return player4Wins;
+    }
+
+    public void setPlayer4Wins(Integer player4Wins) {
+        this.player4Wins = player4Wins;
     }
 
     public Integer getDraws() {
@@ -59,22 +86,89 @@ public class Statistics {
     }
 
     public void setGamesInProgress(Integer gamesInProgress) {
-        gamesInProgress = gamesInProgress;
+        this.gamesInProgress = gamesInProgress;
     }
 
-    public Integer getplayerRank(){
-        return 0;
+    public Integer getPlayerRank(PlayerType player) {
+        switch (player) {
+            case PLAYERONE:
+                return player1Rank;
+            case PLAYERTWO:
+                return player2Rank;
+            case PLAYERTHREE:
+                return player3Rank;
+            case PLAYERFOUR:
+                return player4Rank;
+            default:
+                return 0;
+        }
     }
-    public Integer setplayerRank(Integer playerRank){
-        return 0;
+
+    private void setPlayerRank(PlayerType player, Integer rank) {
+        switch (player) {
+            case PLAYERONE:
+                player1Rank = rank;
+                break;
+            case PLAYERTWO:
+                player2Rank = rank;
+                break;
+            case PLAYERTHREE:
+                player3Rank = rank;
+                break;
+            case PLAYERFOUR:
+                player4Rank = rank;
+                break;
+            case NOPLAYER:
+                break;
+        }
     }
 
     public Integer gettotalScore(){
-        return 0;
+        return totalScore;
     }
 
-    public Integer settotalScore(){
-        return 0;
+    public void settotalScore(Integer totalScore){
+        this.totalScore = totalScore;
     }
 
+ //** new methods **   
+    public void updateTotalScore(int points) {
+        totalScore += points;
+        calculatePlayerRanks();
+    }
+
+    public Integer checkAndAwardPoints(String[][] shownGrid, int[][] coordinatesList) {
+        return checkAndAwardPoint;
+    }
+
+    private void calculatePlayerRanks() {
+        Map<PlayerType, Integer> playerRanks = new HashMap<>();
+        playerRanks.put(PlayerType.PLAYERONE, getPlayer1Wins());
+        playerRanks.put(PlayerType.PLAYERTWO, getPlayer2Wins());
+        playerRanks.put(PlayerType.PLAYERTHREE, getPlayer3Wins());
+        playerRanks.put(PlayerType.PLAYERFOUR, getPlayer4Wins());
+
+        // Sort by values(total points)
+        Map<PlayerType, Integer> sortedPlayerRanks = playerRanks.entrySet().stream()
+                .sorted(Map.Entry.<PlayerType, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, HashMap::new));
+
+        // Update player ranks
+        int rank = 1;
+        for (PlayerType player : sortedPlayerRanks.keySet()) {
+            setPlayerRank(player, rank);
+            rank++;
+        }
+    }
+    public String leaderBoard() {
+        StringBuilder leaderboard = new StringBuilder();
+        leaderboard.append("Leaderboard:\n");
+        leaderboard.append("Player\tWins\tRank\n");
+        leaderboard.append("Player 1\t").append(player1Wins).append("\t").append(player1Rank).append("\n");
+        leaderboard.append("Player 2\t").append(player2Wins).append("\t").append(player2Rank).append("\n");
+        leaderboard.append("Player 3\t").append(player3Wins).append("\t").append(player3Rank).append("\n");
+        leaderboard.append("Player 4\t").append(player4Wins).append("\t").append(player4Rank).append("\n");
+        return leaderboard.toString();
+    }
 }
+ 
