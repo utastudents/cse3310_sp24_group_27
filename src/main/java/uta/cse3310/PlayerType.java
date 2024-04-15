d936ed17b6e609c996936fdbf81b47f031a5416b
Xavior Lopez <xal3586@mavs.uta.edu>
2024-04-15 00:33:11 -0500

    Extra player in enum, deleted extra player

--- src/main/java/uta/cse3310/PlayerType.java
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -3,6 +3,8 @@ package uta.cse3310;
 // A player can be either 1,2,3, or 4 depending on how many are present
 
 public enum PlayerType {
-    NOPLAYER, PLAYERONE, PLAYERTWO, PLAYERTHREE, PLAYERFOUR, OPLAYER
+    NOPLAYER, PLAYERONE, PLAYERTWO, PLAYERTHREE, PLAYERFOUR
+
 }
+ 
  
