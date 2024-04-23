628ede17f7ec764897e2a2f16ed4382d6596ee97
beshoykamel1 <bxk1422@mavs.uta.edu>
2024-04-23 05:46:54 -0500

    errors

--- src/test/java/uta/cse3310/HttpServerTest.java
+++ src/test/java/uta/cse3310/HttpServerTest.java
@@ -1,48 +1,48 @@
-// package uta.cse3310;
+package uta.cse3310;
 
-// import junit.framework.TestCase;
+import junit.framework.TestCase;
 
-// import java.io.IOException;
-// import java.net.HttpURLConnection;
-// import java.net.URL;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-// public class HttpServerTest extends TestCase {
+public class HttpServerTest extends TestCase {
 
-//     private HttpServer server;
-//     private Thread serverThread;
-//     private final int testPort = 8080;
-//     private final String testDirName = "./test-html";
+    private HttpServer server;
+    private Thread serverThread;
+    private final int testPort = 8080;
+    private final String testDirName = "./test-html";
 
-//     protected void setUp() throws Exception {
-//         super.setUp();
-//         server = new HttpServer(testPort, testDirName);
-//         serverThread = new Thread(() -> server.start());
-//         serverThread.start();
-//     }
+    protected void setUp() throws Exception {
+        super.setUp();
+        server = new HttpServer(testPort, testDirName);
+        serverThread = new Thread(() -> server.start());
+        serverThread.start();
+    }
 
-//     protected void tearDown() throws Exception {
-//         super.tearDown();
-//         serverThread.interrupt();
-//         serverThread.join();
-//     }
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        serverThread.interrupt();
+        serverThread.join();
+    }
 
-//     public void testFileServing() throws IOException {
-//         URL url = new URL("http://localhost:" + testPort + "/index.html");
-//         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//         int responseCode = connection.getResponseCode();
-//         assertEquals(200, responseCode);
-//     }
+    public void testFileServing() throws IOException {
+        URL url = new URL("http://localhost:" + testPort + "/index.html");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        int responseCode = connection.getResponseCode();
+        assertEquals(200, responseCode);
+    }
 
-//     public void testAPITimeEndpoint() throws IOException {
-//         URL url = new URL("http://localhost:" + testPort + "/api/time");
-//         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//         int responseCode = connection.getResponseCode();
-//         assertEquals(200, responseCode);
+    public void testAPITimeEndpoint() throws IOException {
+        URL url = new URL("http://localhost:" + testPort + "/api/time");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        int responseCode = connection.getResponseCode();
+        assertEquals(200, responseCode);
 
-//         String contentType = connection.getHeaderField("Content-Type");
-//         assertEquals("text/plain", contentType);
+        String contentType = connection.getHeaderField("Content-Type");
+        assertEquals("text/plain", contentType);
 
-//         String response = new String(connection.getInputStream().readAllBytes());
-//         assertTrue(response.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}"));
-//     }
-// }
+        String response = new String(connection.getInputStream().readAllBytes());
+        assertTrue(response.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}"));
+    }
+}
